edc1a0f852486671c2f86f57ee505a95b56c3fa0
Marcus Valverde <marcusvalverde18@gmail.com>
2021-01-21 19:12:39 -0800

    fixed css bugs and added correct back ticks for template literal, added spaces

--- componentlibrary/src/components/Button/Button.js
+++ componentlibrary/src/components/Button/Button.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import PropTypes from 'prop-types';
+import './Button.css'
 
-const Button = ({children, ...rest})=>{
+export const Button = ({primary,size,backgroundColor,label, ...props}) => {
+  const actionMode = primary ? 'btn--primary' : 'btn--secondary';
   return (
-  <button className='button' {...rest}>{children}</button>
+  <button 
+   type='button'
+   className={['btn ', `btn--${size} `,actionMode].join('')}
+   style={{backgroundColor}}
+   {...props}
+   >
+     {label}
+  </button>
   )
 }
 
-export default Button
+Button.propTypes = {
+  primary: PropTypes.bool,
+  backgroundColor: PropTypes.string,
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  label: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+};
+Button.defaultProps = {
+  backgroundColor: null,
+  primary: true,
+  size: 'medium',
+  onClick: undefined,
+};
